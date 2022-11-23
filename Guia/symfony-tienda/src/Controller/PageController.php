294e52ebff73d1524919884c52ef6b8f04e93e8b
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-11-23 10:39:51 +0100

    1.3 Resto de paginas: terminado

--- Guia/symfony-tienda/src/Controller/PageController.php
+++ Guia/symfony-tienda/src/Controller/PageController.php
@@ -9,9 +9,36 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/page', name: 'app_page')]
-    public function index(): Response
-    {
+    public function index(): Response{
         return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+
+    #[Route('/about', name: 'about')]
+    public function about(): Response{
+        return $this->render('page/about.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response{
+        return $this->render('page/contact.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+
+    #[Route('/service', name: 'service')]
+    public function service(): Response{
+        return $this->render('page/service.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+
+    #[Route('/team', name: 'team')]
+    public function team(): Response{
+        return $this->render('page/team.html.twig', [
             'controller_name' => 'PageController',
         ]);
     }
---
+++ Guia/symfony-tienda/src/Controller/ProductController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product', name: 'app_product')]
+    public function product(): Response{
+        return $this->render('product/product.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    #[Route('/product/price', name: 'price')]
+    public function price(): Response{
+        return $this->render('product/price.html.twig', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+}

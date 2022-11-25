d4fe3d1e518b887d720ab8bdee3be479d5736480
AlejandrosMiralles <alumir3453@ieselcaminas.org>
2022-11-25 12:04:18 +0100

    2.3 Productos: terminado

--- Guia/symfony-tienda/src/Controller/ProductController.php
+++ Guia/symfony-tienda/src/Controller/ProductController.php
@@ -3,20 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Producto;
+use App\Services\ProductsService;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-use Doctrine\Persistence\ManagerRegistry;
-
 class ProductController extends AbstractController
 {
     #[Route('/product', name: 'product')]
-    public function product(): Response{
-        return $this->render('product/product.html.twig', [
-            'controller_name' => 'ProductController',
-        ]);
+    public function product(ProductsService $productsService): Response{
+        $products = $productsService->getProducts();
+        return $this->render('product/product.html.twig', compact('products'));
     }
 
     #[Route('/product/price', name: 'price')]
@@ -24,13 +22,5 @@ class ProductController extends AbstractController
         return $this->render('product/price.html.twig', [
             'controller_name' => 'ProductController',
         ]);
-    }
-
-    public function productTemplate(ManagerRegistry $doctrine): Response{
-        $repository = $doctrine->getRepository(Producto::class);
-        $products = $repository->findAll();
-        return $this->render('partials/_product.html.twig', 
-            compact('products')
-        );
     }
 }
